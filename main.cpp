e9e2bbd44aa17ae4fd8b5e8702eb6202361963e0
Cliff McCartney <caiks@greenlake.co.uk>
2019-10-25 10:53:16 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -298,7 +298,7 @@ int main(int argc, char **argv)
 
     }
 
-    if (true)
+    if (false)
     {
 	auto uvars = systemsSetVar;
 	auto vol = systemsSetVarsVolume_u;
@@ -451,6 +451,259 @@ int main(int argc, char **argv)
 	    2187.68,[<14,8>,<15,8>]
 	    2234.05,[<14,9>,<15,9>]
 	    2291.64,[<13,9>,<14,9>]
+	    */
+	}
+    }
+
+    if (true)
+    {
+	auto uvars = systemsSetVar;
+	auto vol = systemsSetVarsVolume_u;
+	auto aall = histogramsList;
+	auto cart = systemsSetVarsSetStateCartesian_u;
+	auto single = histogramSingleton_u;
+	auto aaar = systemsHistogramsHistogramRepa_u;
+	auto araa = systemsHistogramRepasHistogram_u;
+	auto aahr = [](const System& uu, const SystemRepa& ur, const Histogram& aa)
+	{
+	    return systemsHistoriesHistoryRepa_u(uu, ur, *histogramsHistory_u(aa));
+	};
+	auto hrsel = [](const HistoryRepa& hr, const SizeList& ll)
+	{
+	    return eventsHistoryRepasHistoryRepaSelection_u(ll.size(), (std::size_t*)ll.data(), hr);
+	};
+	auto hrhrsel = [](const HistoryRepa& hr, const HistoryRepa& ss)
+	{
+	    return historyRepasHistoryRepasHistoryRepaSelection_u(ss, hr);
+	};
+	auto hrred = [](const HistoryRepa& hr, const SystemRepa& ur, const VarUSet& kk)
+	{
+	    auto& vvi = ur.mapVarSize();
+	    std::size_t m = kk.size();
+	    auto kk0 = sorted(kk);
+	    SizeList kk1;
+	    for (auto& v : kk0)
+		kk1.push_back(vvi[v]);
+	    return setVarsHistoryRepasReduce_u(1.0, m, kk1.data(), hr);
+	};
+	auto hrhrred = [](const HistoryRepa& hr, const SystemRepa& ur, const VarUSet& kk)
+	{
+	    auto& vvi = ur.mapVarSize();
+	    std::size_t m = kk.size();
+	    auto kk0 = sorted(kk);
+	    SizeList kk1;
+	    for (auto& v : kk0)
+		kk1.push_back(vvi[v]);
+	    return setVarsHistoryRepasHistoryRepaReduced_u(m, kk1.data(), hr);
+	};
+
+	auto xx = trainBucketedIO(2);
+	auto& uu = std::get<0>(xx);
+	auto& ur = std::get<1>(xx);
+	auto& hrtr = std::get<2>(xx);
+
+	Variable digit("digit");
+	auto vv = *uvars(*uu);
+	auto vvl = VarUSet();
+	vvl.insert(digit);
+	auto vvk = VarUSet(vv);
+	vvk.erase(digit);
+
+	cout << "hrtr->dimension" << endl
+	    << hrtr->dimension << endl << endl;
+	cout << "hrtr->size" << endl
+	    << hrtr->size << endl << endl;
+
+	SizeList ll;
+	for (size_t i = 0; i < hrtr->size; i += 8)
+	    ll.push_back(i);
+	auto hr = hrsel(*hrtr, ll);
+	cout << "hr->dimension" << endl
+	    << hr->dimension << endl << endl;
+	cout << "hr->size" << endl
+	    << hr->size << endl << endl;
+
+	auto& vvi = ur->mapVarSize();
+	auto vvk0 = sorted(vvk);
+	SizeList vvk1;
+	for (auto& v : vvk0)
+	    vvk1.push_back(vvi[v]);
+
+	size_t xmax = 4096;
+	size_t omax = 10;
+	{
+	    auto start = chrono::system_clock::now();
+	    auto hrs = historyRepasShuffle_u(*hr, 1);
+	    auto end = chrono::system_clock::now();
+	    cout << "historyRepasShuffle_u " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto pr = historyRepasRed(*hr);
+	    end = chrono::system_clock::now();
+	    cout << "historyRepasRed(hr) " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto prs = historyRepasRed(*hrs);
+	    end = chrono::system_clock::now();
+	    cout << "historyRepasRed(hrs) " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto xx = parametersSetVarsHistoryRepasSetSetVarsAlignedTop_u(xmax, omax, vvk1, *hr, *pr, *hrs, *prs);
+	    end = chrono::system_clock::now();
+	    cout << "hrcross " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+	    auto xc = std::move(std::get<0>(xx));
+	    auto sc = std::get<1>(xx);
+	    cout << "steps: " << sc << endl;
+	    auto tt = sorted(*xc);
+	    for (auto& mm : tt)
+	    {
+		cout << mm.first << ", ";
+		VarList ll1;
+		for (auto& i : mm.second)
+		    ll1.push_back((ur->listVarUCharPair[i]).first);
+		cout << ll1 << endl;
+	    }
+
+	    SizeListList vdd;
+	    for (auto& mm : *xc)
+		vdd.push_back(mm.second);
+
+	    start = chrono::system_clock::now();
+	    xx = parametersSetVarsSetSetVarsHistoryRepasSetSetVarsAlignedTop_u(xmax, omax, vvk1, vdd, *hr, *pr, *hrs, *prs);
+	    end = chrono::system_clock::now();
+	    cout << "hrappend " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    xc = std::move(std::get<0>(xx));
+	    sc = std::get<1>(xx);
+	    cout << "steps: " << sc << endl;
+	    tt = sorted(*xc);
+	    for (auto& mm : tt)
+	    {
+		cout << mm.first << ", ";
+		VarList ll1;
+		for (auto& i : mm.second)
+		    ll1.push_back((ur->listVarUCharPair[i]).first);
+		cout << ll1 << endl;
+	    }
+	    /*
+	    historyRepasShuffle_u 0.234003s
+	    historyRepasRed(hr) 0.0156002s
+	    historyRepasRed(hrs) 0.0156002s
+	    hrcross 13.9778s
+	    steps: 306936
+	    2123.91, [<24,12>,<24,13>]
+	    2123.98, [<17,20>,<18,20>]
+	    2130.76, [<15,9>,<16,9>]
+	    2132.73, [<12,10>,<13,10>]
+	    2134.58, [<23,12>,<23,13>]
+	    2136.53, [<23,11>,<23,12>]
+	    2172.46, [<12,9>,<13,9>]
+	    2187.93, [<14,8>,<15,8>]
+	    2234.01, [<14,9>,<15,9>]
+	    2292.43, [<13,9>,<14,9>]
+	    hrappend 0.81121s
+	    steps: 7820
+	    4175.65, [<23,9>,<23,11>,<23,12>]
+	    4181.65, [<11,8>,<12,9>,<13,9>]
+	    4201.86, [<19,19>,<17,20>,<18,20>]
+	    4260.35, [<16,7>,<14,8>,<15,8>]
+	    4268.39, [<14,9>,<12,10>,<13,10>]
+	    4275.66, [<13,7>,<14,8>,<15,8>]
+	    4297.26, [<23,10>,<23,12>,<23,13>]
+	    4383.27, [<14,8>,<15,9>,<16,9>]
+	    4476.49, [<12,8>,<13,9>,<14,9>]
+	    4542.22, [<13,8>,<14,9>,<15,9>]
+	    */
+	}
+
+	{
+	    auto start = chrono::system_clock::now();
+	    hr->transpose();
+	    auto end = chrono::system_clock::now();
+	    cout << "transpose " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto hrs = historyRepasShuffle_u(*hr, 1);
+	    end = chrono::system_clock::now();
+	    cout << "historyRepasShuffle_u " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto pr = historyRepasRed(*hr);
+	    end = chrono::system_clock::now();
+	    cout << "historyRepasRed(hr) " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto prs = historyRepasRed(*hrs);
+	    end = chrono::system_clock::now();
+	    cout << "historyRepasRed(hrs) " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    start = chrono::system_clock::now();
+	    auto xx = parametersSetVarsHistoryRepasSetSetVarsAlignedTop_u(xmax, omax, vvk1, *hr, *pr, *hrs, *prs);
+	    end = chrono::system_clock::now();
+	    cout << "hrcross " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+	    auto xc = std::move(std::get<0>(xx));
+	    auto sc = std::get<1>(xx);
+	    cout << "steps: " << sc << endl;
+	    auto tt = sorted(*xc);
+	    for (auto& mm : tt)
+	    {
+		cout << mm.first << ", ";
+		VarList ll1;
+		for (auto& i : mm.second)
+		    ll1.push_back((ur->listVarUCharPair[i]).first);
+		cout << ll1 << endl;
+	    }
+
+	    SizeListList vdd;
+	    for (auto& mm : *xc)
+		vdd.push_back(mm.second);
+
+	    start = chrono::system_clock::now();
+	    xx = parametersSetVarsSetSetVarsHistoryRepasSetSetVarsAlignedTop_u(xmax, omax, vvk1, vdd, *hr, *pr, *hrs, *prs);
+	    end = chrono::system_clock::now();
+	    cout << "hrappend " << ((chrono::duration<double>)(end - start)).count() << "s" << endl;
+
+	    xc = std::move(std::get<0>(xx));
+	    sc = std::get<1>(xx);
+	    cout << "steps: " << sc << endl;
+	    tt = sorted(*xc);
+	    for (auto& mm : tt)
+	    {
+		cout << mm.first << ", ";
+		VarList ll1;
+		for (auto& i : mm.second)
+		    ll1.push_back((ur->listVarUCharPair[i]).first);
+		cout << ll1 << endl;
+	    }
+	    /*
+	    transpose 0.0312004s
+	    historyRepasShuffle_u 0.390005s
+	    historyRepasRed(hr) 0s
+	    historyRepasRed(hrs) 0s
+	    hrcross 104.007s
+	    steps: 306936
+	    2123.2, [<24,12>,<24,13>]
+	    2123.24, [<17,20>,<18,20>]
+	    2130.83, [<15,9>,<16,9>]
+	    2132.78, [<12,10>,<13,10>]
+	    2134.47, [<23,12>,<23,13>]
+	    2138.44, [<23,11>,<23,12>]
+	    2172.53, [<12,9>,<13,9>]
+	    2187.68, [<14,8>,<15,8>]
+	    2234.05, [<14,9>,<15,9>]
+	    2291.64, [<13,9>,<14,9>]
+	    hrappend 6.06848s
+	    steps: 7820
+	    4178.08, [<23,9>,<23,11>,<23,12>]
+	    4181.29, [<11,8>,<12,9>,<13,9>]
+	    4202.66, [<19,19>,<17,20>,<18,20>]
+	    4260.41, [<16,7>,<14,8>,<15,8>]
+	    4269.11, [<14,9>,<12,10>,<13,10>]
+	    4275.52, [<13,7>,<14,8>,<15,8>]
+	    4298.27, [<23,10>,<23,12>,<23,13>]
+	    4383.27, [<14,8>,<15,9>,<16,9>]
+	    4475.37, [<12,8>,<13,9>,<14,9>]
+	    4543.04, [<13,8>,<14,9>,<15,9>]
 	    */
 	}
     }
