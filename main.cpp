696ce2f9f3c2aaec9cd5089f7fdf6e5d49f24f9c
Cliff McCartney <caiks@greenlake.co.uk>
2019-12-13 12:07:28 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1981,7 +1981,7 @@ int main(int argc, char **argv)
 	}
     }
 
-    if (false || (argc >= 2 && string(argv[1]) == "model35_induce"))
+    if (argc >= 3 && string(argv[1]) == "induce" && string(argv[2]) == "model35")
     {
 	auto uvars = systemsSetVar;
 	auto hrsel = [](const HistoryRepa& hr, const SizeList& ll)
@@ -2041,7 +2041,7 @@ int main(int argc, char **argv)
 
     }
 
-    if (false || (argc >= 2 && string(argv[1]) == "model100_induce"))
+    if (argc >= 3 && string(argv[1]) == "induce" && string(argv[2]) == "model100")
     {
 	auto uvars = systemsSetVar;
 	auto hrsel = [](const HistoryRepa& hr, const SizeList& ll)
@@ -2208,7 +2208,7 @@ int main(int argc, char **argv)
 	bmwrite("NIST01.bmp", bmvstack(ll2));
     }
 
-    if (false || (argc >= 2 && string(argv[1]) == "model35_bitmap"))
+    if (argc >= 3 && string(argv[1]) == "bitmap" && string(argv[2]) == "model35")
     {
 	auto uvars = systemsSetVar;
 	auto single = histogramSingleton_u;
@@ -2315,7 +2315,7 @@ int main(int argc, char **argv)
 	bmwrite("NIST_model35.bmp", bmvstack(ll2));
     }
 
-    if (false || (argc >= 2 && string(argv[1]) == "model100_bitmap"))
+    if (argc >= 3 && string(argv[1]) == "bitmap" && string(argv[2]) == "model100")
     {
 	auto uvars = systemsSetVar;
 	auto single = histogramSingleton_u;
@@ -5812,7 +5812,7 @@ int main(int argc, char **argv)
 	out.close();
     }
 
-    if (true)
+    if (false)
     {
 	auto uvars = systemsSetVar;
 	auto hrsel = eventsHistoryRepasHistoryRepaSelection_u;
@@ -5910,6 +5910,155 @@ int main(int argc, char **argv)
 	systemRepasPersistent(*ur, out); cout << endl;
 	applicationRepasPersistent(*dr3, out); cout << endl;
 	out.close();
+    }
+
+    if (argc >= 3 && string(argv[1]) == "test")
+    {
+	auto uvars = systemsSetVar;
+	auto single = histogramSingleton_u;
+	auto aahr = [](const System& uu, const SystemRepa& ur, const Histogram& aa)
+	{
+	    return systemsHistoriesHistoryRepa_u(uu, ur, *histogramsHistory_u(aa));
+	};
+	auto hrsel = eventsHistoryRepasHistoryRepaSelection_u;
+	auto hrhrred = setVarsHistoryRepasHistoryRepaReduced_u;
+	auto hrred = setVarsHistoryRepasReduce_u;
+	auto frmul = historyRepasFudRepasMultiply_u;
+	auto frvars = fudRepasSetVar;
+	auto frder = fudRepasDerived;
+	auto frund = fudRepasUnderlying;
+	auto frdep = fudRepasSetVarsDepends;
+
+	string model = string("NIST_") + string(argv[2]);
+
+	cout << "model: " << model << endl;
+
+	std::unique_ptr<System> uu;
+	std::unique_ptr<SystemRepa> ur;
+	std::unique_ptr<HistoryRepa> hr;
+	{
+	    auto xx = trainBucketedIO(2);
+	    uu = std::move(std::get<0>(xx));
+	    ur = std::move(std::get<1>(xx));
+	    hr = std::move(std::get<2>(xx));
+	}
+
+	Variable digit("digit");
+	auto vv = *uvars(*uu);
+	auto vvl = VarUSet();
+	vvl.insert(digit);
+	auto vvk = VarUSet(vv);
+	vvk.erase(digit);
+
+	auto& vvi = ur->mapVarSize();
+	SizeList vvk1;
+	for (auto& v : sorted(vvk))
+	    vvk1.push_back(vvi[v]);
+
+	SizeList vvl1{ vvi[digit] };
+
+	cout << "train size: " << hr->size << endl;
+
+	StrVarPtrMap m;
+	std::ifstream in(model + ".bin", std::ios::binary);
+	auto ur1 = persistentsSystemRepa(in, m);
+	auto dr = persistentsApplicationRepa(in);
+	in.close();
+
+	cout << "model cardinality: " << fudRepasSize(*dr->fud) << endl;
+	cout << "derived  cardinality: " << frder(*dr->fud)->size() << endl;
+	cout << "underlying cardinality: " << frund(*dr->fud)->size() << endl;
+
+	std::map<std::size_t, std::size_t> sp;
+	{
+	    auto hr1 = frmul(*hr, *dr->fud);
+	    if (hr1->evient)
+		hr1->transpose();
+	    auto z = hr1->size;
+	    auto& mvv = hr1->mapVarInt();
+	    auto sh = hr1->shape;
+	    auto rr = hr1->arr;
+	    auto pl = mvv[vvi[digit]];
+	    auto sl = sh[pl];
+	    auto nn = treesLeafNodes(*dr->slices);
+	    SizeList al(sl);
+	    for (auto& s : *nn)
+	    {
+		for (std::size_t k = 0; k < sl; k++)
+		    al[k] = 0;
+		auto pk = mvv[s.first];
+		for (std::size_t j = 0; j < z; j++)
+		{
+		    std::size_t u = rr[pk*z + j];
+		    if (u)
+		    {
+			std::size_t w = rr[pl*z + j];
+			al[w]++;
+		    }
+		}
+		std::size_t c = 0;
+		std::size_t cl = sl;
+		for (std::size_t k = 0; k < sl; k++)
+		{
+		    auto u = al[k];
+		    if (u > c)
+		    {
+			c = u;
+			cl = k;
+		    }
+		}
+		sp[s.first] = cl;
+	    }
+
+	}
+	//cout << "sp" << endl
+	//    << sp << endl << endl;
+
+	{
+	    auto xx = testBucketedIO(2);
+	    hr = std::move(std::get<2>(xx));
+	}
+
+	cout << "test size: " << hr->size << endl;
+
+	std::size_t a = 0;
+	std::size_t x = 0;
+	{
+	    SizeList ww{ vvi[digit] };
+	    auto nn = treesLeafNodes(*dr->slices);
+	    for (auto& s : *nn)
+		ww.push_back(s.first);
+	    auto hr1 = hrhrred(ww.size(), ww.data(), *frmul(*hr, *dr->fud));
+	    if (!hr1->evient)
+		hr1->transpose();
+	    auto n = hr1->dimension;
+	    auto vv = hr1->vectorVar;
+	    auto sh = hr1->shape;
+	    auto z = hr1->size;
+	    auto rr = hr1->arr;
+	    auto sl = sh[0];
+	    for (std::size_t j = 0; j < z; j++)
+	    {
+		std::size_t cl = rr[j*n];
+		for (std::size_t i = 1; i < n; i++)
+		{
+		    std::size_t u = rr[j*n + i];
+		    if (u)
+		    {
+			std::size_t l = sp[vv[i]];
+			if (l == cl)
+			    a++;
+			else if (l == sl)
+			    x++;
+			break;
+		    }
+		}
+	    }
+
+	}
+	cout << "effective size: " << hr->size - x << endl;
+	cout << "matches: " << a << endl;
+
     }
 
 
